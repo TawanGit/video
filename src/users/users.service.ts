e0d7476824f7f32fb6a9fc741b93a31e54b447bc
tawan <thiago_tawan@outlook.com>
2025-08-08 16:14:47 -0300

    feat: adding validations e bcrypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/User';
 import { Repository } from 'typeorm';
 import { UserDto } from './dto/user-dto';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
   constructor(
@@ -12,7 +12,26 @@ export class UsersService {
   fetchUsers() {
     return this.userRepository.find();
   }
-  createUser(user: UserDto) {
-    return this.userRepository.save(user);
+  async createUser(user: UserDto) {
+    const userExist = await this.userRepository.findOneBy({
+      email: user.email,
+    });
+
+    const usernameExist = await this.userRepository.findOneBy({
+      username: user.username,
+    });
+    if (userExist) {
+      throw new ConflictException('Email já existe.');
+    }
+
+    if (usernameExist) {
+      throw new ConflictException('Username já existe');
+    }
+
+    const hashPassword = await bcrypt.hash(user.password, 10);
+    return this.userRepository.save({
+      ...user,
+      password: hashPassword,
+    });
   }
 }
